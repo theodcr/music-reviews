4325e25b0d0d7f814f6b0dba32d8ab1bdf5fc0d6
Théo <theo.delecour@gmx.com>
2018-11-03 17:09:37 +0100

    fix config file paths

--- music-reviews/config.py
+++ music-reviews/config.py
@@ -1,4 +1,9 @@
 from configparser import ConfigParser
+import os
+
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(os.path.abspath(__file__), os.path.pardir, os.path.pardir)
+)
 
 CONFIG = ConfigParser()
-CONFIG.read('config.cfg')
+CONFIG.read(os.path.join(PROJECT_ROOT, 'config', 'config.cfg'))
