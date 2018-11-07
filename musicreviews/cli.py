ee1d1f44c61a5b1dba89e75cda42a74963acae0a
Théo <theo.delecour@gmx.com>
2018-11-08 00:19:26 +0100

    artist album list and prompt

--- musicreviews/cli.py
+++ musicreviews/cli.py
@@ -8,7 +8,7 @@ from functools import partial
 import click
 from musicreviews import creation, generation, helpers
 from musicreviews.config import CONFIG
-from powerspot.operations import search_artist
+from powerspot.operations import get_artist_albums, search_artist
 
 MIN_RATING = int(CONFIG['creation']['min_rating'])
 MAX_RATING = int(CONFIG['creation']['max_rating'])
@@ -46,13 +46,13 @@ def generate(ctx):
 def create(ctx, uri):
     if uri == "":
         known_artists = [x['artist'] for x in ctx.obj['albums']]
-        artist_query = helpers.completion_input(style_prompt("Artist search"), known_artists)
-        results = search_artist(artist_query)['items']
-        artists = [results[i]['name'] for i in range(len(results))]
+        artist_query = helpers.completion_input(
+            style_prompt("Artist search"), known_artists
+        )
+        res_artists = search_artist(artist_query)['items']
+        artists = [res_artists[i]['name'] for i in range(len(res_artists))]
         for i, artist in enumerate(artists):
-            click.echo(
-                click.style(str(i), fg='magenta') + ' ' + click.style(artist, fg='blue')
-            )
+            click.echo(style_enumerate(i, artist))
         artist_id = click.prompt(
             style_prompt("Choose artist index"),
             value_proc=partial(
@@ -60,6 +60,20 @@ def create(ctx, uri):
             ),
             default=0,
         )
+        artist_uri = res_artists[artist_id]['uri']
+        res_albums = get_artist_albums(artist_uri)['items']
+        albums = [res_albums[i]['name'] for i in range(len(res_albums))]
+        for i, album in enumerate(albums):
+            click.echo(style_enumerate(i, album))
+        album_id = click.prompt(
+            style_prompt("Choose album index"),
+            value_proc=partial(
+                helpers.check_integer_input, min_value=0, max_value=len(albums) - 1
+            ),
+            default=0,
+        )
+        album_data = res_albums[album_id]
+        album_uri = album_data['uri']
     click.prompt(
         style_prompt("Rating"),
         value_proc=partial(
@@ -75,6 +89,11 @@ def create(ctx, uri):
 def style_prompt(message):
     """Returns a unified style for click prompts"""
     return click.style(message, fg='cyan')
+
+
+def style_enumerate(i, val):
+    """Returns a unified style for enumerate items"""
+    return click.style(str(i), fg='magenta') + ' ' + click.style(val, fg='blue')
 
 
 if __name__ == '__main__':
