4a64aa08c862bc5e6a30f5f6d44c6fbeb1ed0549
Théo Delecour <theo.delecour@gmx.com>
2019-04-26 20:02:51 +0200

    Implement bold and italic conversion for html

--- musicreviews/exporter.py
+++ musicreviews/exporter.py
@@ -62,6 +62,8 @@ def wiki_to_markdown(string):
 
 def wiki_to_html(string):
     """Translates the string from vimwiki format to HTML."""
+    string = replace_enclosed_text_tags(string, '\*', '<b>', '</b>')
+    string = replace_enclosed_text_tags(string, '_', '<i>', '</i>')
     return string
 
 
