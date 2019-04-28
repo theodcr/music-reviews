55f78afdff21d369ca55f0816dfc2eef97ee12f8
Théo Delecour <theo.delecour@gmx.com>
2019-04-28 20:08:06 +0200

    Delete empty utils module

--- musicreviews/__init__.py
+++ musicreviews/__init__.py
@@ -2,18 +2,18 @@ from . import cli
 from . import configuration
 from . import creator
 from . import exporter
+from . import formatter
 from . import indexer
 from . import reader
 from . import ui
-from . import utils
 
 __all__ = [
     'cli',
     'configuration',
     'creator',
     'exporter',
+    'formatter',
     'indexer',
     'reader',
     'ui',
-    'utils',
 ]
